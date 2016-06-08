72ffc888575cc01f4150f74b4c590b16188c5757
Tyler Butler <tylerbu@microsoft.com>
2016-06-08 15:32:39 -0700

    Rename functions and variables to be consistent with PEP8.

--- WopiValidatorExecutor/__main__.py
+++ WopiValidatorExecutor/__main__.py
@@ -10,19 +10,19 @@ from .constants import *
 def main():
     # Read Executor Config.
     config = configparser.ConfigParser()
-    EXECUTOR_CONFIG_FILE = resource_filename('WopiValidatorExecutor.WopiValidatorExecutor', 'ExecutorConfig.ini')
-    config.read(EXECUTOR_CONFIG_FILE)
+    config_file = resource_filename('WopiValidatorExecutor.WopiValidatorExecutor', 'ExecutorConfig.ini')
+    config.read(config_file)
 
-    defaultWopiDiscoveryServiceUrl = config['DEFAULT'][DEFAULT_WOPIDISCOVERYSERVICEURL]
-    defaultTestCategory = config['DEFAULT'][DEFAULT_TESTCATEGORY]
+    default_wopi_discovery_service_url = config['DEFAULT'][DEFAULT_WOPIDISCOVERYSERVICEURL]
+    default_test_category = config['DEFAULT'][DEFAULT_TESTCATEGORY]
 
     # Read command line arguments.
     parser = argparse.ArgumentParser(description="WopiValidator Execution Program")
     parser.add_argument('wopisrc', type=str, help="Defines the unencoded WopiSrc Url of .WopiTest file.")
     parser.add_argument('accesstoken', type=str, help="Defines a valid WopiAccessToken.")
-    parser.add_argument('-c', type=str, default=defaultTestCategory,
+    parser.add_argument('-c', type=str, default=default_test_category,
                         help="Defines the target TestCategory, It could either be All, OfficeOnline or OfficeNativeClient and is case-insensitive.")
-    parser.add_argument('-s', type=str, default=defaultWopiDiscoveryServiceUrl,
+    parser.add_argument('-s', type=str, default=default_wopi_discovery_service_url,
                         help="This represents the WOPI Discovery Service Url for OfficeOnline.")
     parser.add_argument('-v', action="store_const", dest='logLevel', const=logging.DEBUG, default=logging.WARNING,
                         help="Turns on verbose logging")
@@ -39,6 +39,6 @@ def main():
                'access_token_ttl': '0'}
 
     # Execute the WopiValidator.
-    from .WopiValidatorExecutor import ExecuteWopiValidator
+    from .WopiValidatorExecutor import execute_wopi_validator
 
-    ExecuteWopiValidator(args.s, payload)
+    execute_wopi_validator(args.s, payload)
